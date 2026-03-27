79fe9d1b56fb662647c430dfdd40a7824565147d
OrangeDrangon <25403517+OrangeDrangon@users.noreply.github.com>
2026-03-27 16:15:09 -0400

    port to modern django esi
    have not run tests. manual testing seems good. tests probably wont work
    because it uses some other source of stubs

--- buybackprogram/providers.py
+++ buybackprogram/providers.py
@@ -1,16 +1,16 @@
-import os
+from esi.openapi_clients import ESIClientProvider
 
-from esi.clients import EsiClientProvider
+from . import __title__, __version__
 
-from . import __version__
-
-
-def get_swagger_spec_path() -> str:
-    """returns the path to the current swagger spec file"""
-    return os.path.join(os.path.dirname(os.path.abspath(__file__)), "swagger.json")
-
-
-esi = EsiClientProvider(
-    spec_file=get_swagger_spec_path(),
-    app_info_text=f"aa-buyback-program v{__version__}",
+esi = ESIClientProvider(
+    compatibility_date="2025-12-16",
+    ua_appname=__title__,
+    ua_version=__version__,
+    operations=[
+        "GetCorporationsCorporationIdContracts",
+        "GetCorporationsCorporationIdContractsContractIdItems",
+        "GetCharactersCharacterIdContracts",
+        "GetCharactersCharacterIdContractsContractIdItems",
+        "GetUniverseStructuresStructureId",
+    ],
 )
